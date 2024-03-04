5f143483030195b69f0b74939fa8c670c6472ea1
JaminLiang721 <157277209+JaminLiang721@users.noreply.github.com>
2024-03-04 02:12:50 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -138,13 +138,10 @@ def add_review(request):
     if request.user.is_anonymous is False:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            # response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401,
                                  "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
-
-response = some_function()
-print(response)
