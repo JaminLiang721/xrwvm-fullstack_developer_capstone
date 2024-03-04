14b47ad7a3d53907db243d7e4c9fe86eaf3c197a
JaminLiang721 <157277209+JaminLiang721@users.noreply.github.com>
2024-03-04 02:22:25 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.contrib import messages
+from datetime import datetime
 from .models import CarMake, CarModel
 
 from django.http import JsonResponse
@@ -138,7 +138,7 @@ def add_review(request):
     if request.user.is_anonymous is False:
         data = json.loads(request.body)
         try:
-            # response = post_review(data)
+            response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401,
